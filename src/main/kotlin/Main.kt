e6258b8d62e2089567b3fddf2ce03460df4724ed
Douglas Ronne <vextorspace@gmail.com>
2024-11-05 18:49:44 -0800

    working

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,5 @@ package com.ronnev
 
 fun main(args: Array<String>? = null) {
     val toWhom = args?.getOrNull(0) ?: "World"
-    println(Greeting(toWhom).greet())
+    println(Greeting().greet(toWhom))
 }
