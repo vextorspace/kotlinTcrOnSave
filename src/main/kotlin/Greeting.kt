61e52184d7a7e265ac4b0474829ce8593d1cbd2e
Douglas Ronne <vextorspace@gmail.com>
2024-11-05 16:02:47 -0800

    working

--- src/main/kotlin/Greeting.kt
+++ src/main/kotlin/Greeting.kt
@@ -3,9 +3,9 @@ package com.ronnev
 import io.kotest.core.spec.style.ExpectSpec
 import io.kotest.matchers.shouldBe
 
-class Greeting {
+class Greeting(val toWhom: String = "World") {
     fun greet(): String {
-        return "Hello World!"
+        return "Hello $toWhom!"
     }
 }
 
