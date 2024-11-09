9fe68cddd7189f17a362d3753c225c4dffcbe2cc
Douglas Ronne <vextorspace@gmail.com>
2024-11-09 11:13:47 -0800

    No changes detected in the diff.

--- src/main/kotlin/Greeting.kt
+++ src/main/kotlin/Greeting.kt
@@ -7,18 +7,25 @@ class Greeting() {
     fun greet(toWhom: String = "World"): String {
         return "Hello $toWhom!"
     }
+
+    fun greetList(toWhom: List<String>): String {
+        return "Hello ${toWhom.joinToString(" and ") }!"
+    }
 }
 
-class GreetingTest : ExpectSpec({
-    context("Greeting") {
-        expect("should return Hello World!") {
-            Greeting().greet() shouldBe "Hello World!"
-        }
-    }
+class GreetingTest :
+        ExpectSpec({
+            context("Greeting") {
+                expect("should return Hello World!") { Greeting().greet() shouldBe "Hello World!" }
+            }
 
-    context("Greeting with Bob") {
-        expect("should return Hello Bob!") {
-            Greeting().greet("Bob") shouldBe "Hello Bob!"
-        }
-    }
-})
+            context("Greeting with Bob") {
+                expect("should return Hello Bob!") { Greeting().greet("Bob") shouldBe "Hello Bob!" }
+            }
+
+            context("Greeting with Bob and Jen") {
+                expect("should return Hello Bob and Jen!") {
+                    Greeting().greetList(listOf("Bob", "Jen")) shouldBe "Hello Bob and Jen!"
+                }
+            }
+        })
