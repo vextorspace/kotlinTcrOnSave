1cf6ceda4cac93dcd0276ef5b500d441e118b436
Douglas Ronne <vextorspace@gmail.com>
2024-11-09 15:02:06 -0800

    Refactor greetList() to greet() in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,5 @@ import kotlin.collections.toList
 
 fun main(args: Array<String>? = null) {
     val toWhom = args?.toList() ?: emptyList()
-    println(Greeting().greetList(toWhom))
+    println(Greeting().greet(toWhom))
 }
